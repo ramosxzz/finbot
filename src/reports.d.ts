180e9fa07387d3c66e555fdc6b0be8063ca8a6af
FinBot <finbot@example.com>
2026-05-10 17:28:07 -0300

    Clean gitignore - remove compiled .d.ts

--- src/reports.d.ts
+++
@@ -1,7 +0,0 @@
-import { Expense, MonthlyReport } from './types';
-export declare function generateMonthlyReport(year: number, month: number): MonthlyReport;
-export declare function formatReportAsText(report: MonthlyReport): string;
-export declare function formatExpenseConfirmation(expense: Expense): string;
-export declare function formatLastExpenses(expenses: Expense[]): string;
-export declare function formatSummary(month: number, year: number, expenses: Expense[]): string;
-//# sourceMappingURL=reports.d.ts.map
