f789a656c035ed728b6b13c25c1475188bcae0b6
FinBot <finbot@example.com>
2026-05-10 17:17:08 -0300

    Fix puppeteer for Render - use bundled Chromium

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,17 @@ const client = new Client({
     dataPath: '.wwebjs_auth'
   }),
   puppeteer: {
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu'
+    ],
+    headless: true
   }
 });
 
